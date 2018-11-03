6f2adb1a0ac1dfc4aa46eff71dbc507bdf2fcd8c
MujurID <40834757+MujurID@users.noreply.github.com>
2018-11-03 08:08:13 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -5,12 +5,14 @@ curl_setopt($ch, CURLOPT_URL, "https://geevv.com/search?q=MujurID&type=web");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HEADER, 1);
 $headers = array();
-$headers[] = "Host: geevv.com";
-$headers[] = "User-Agent: Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36";
-$headers[] = "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7";
-$headers[] = "Referer: https://geevv.com/search?q=MujurID&type=web";
-$headers[] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
-$headers[] = "Cookie: __cfduid=d539cbb77af2174bc4c6e56fe0cdb30711541063620; __smVID=c9e0b994b5228a295dd8b62623a5b516d7bbd6626f6c955483526c1630d807d3; _ga=GA1.2.749746428.1541063625; _gid=GA1.2.33532824.1541063625; geevvSession=c1f7c4d1583731125b5d808a5c4099db; dashboard-popup=1; _fbc=fb.1.1541096421443.IwAR2bjpDPz8jUV4Gk8s1nHeg67fWkakpKTATDMUgN-weEQFCz6RsKBkTv0p4; _fbp=fb.1.1541096421450.111367119; __smToken=0PKlplzMthAjCRIGHzaWYfUB; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6IlFPWmFIOUVMUk5sWVFpU0JtWko1VHc9PSIsInZhbHVlIjoiQkdVdkZ4YVNLbmNNbzFkeEhHZ2dnY2RMejE2em1QMlJYdzUzRW5iMzZoaVdWd0lBNllzdk4xaWRPcEZvUnVcL3EyYUU5bWxnQWZnZk41Y1huSkFPMEZ6NDhsS3NRXC9kdXZKVExLVlREalc4WT0iLCJtYWMiOiJhY2MzZDk3YjIxNzYyYTgwMjMxNjMyYTMxZGE4YzQyMDkyYTQwZTI2M2JhOWRlNjllOTU0ZWRkMTJlMjlhNTE5In0%3D; laravel_session=e8f081f327693976d282b92e5ce21e62b45c7f1c; XSRF-TOKEN=eyJpdiI6IjU2REFEaUZcL3JHdVwvYVNtY3lFQUVcL3c9PSIsInZhbHVlIjoibmtEaGhKSWJ2eFprbXFcL1Y3SHNlVU5xSTRrUEZiaXR5QmNkSFZGMTdTWFdlSHZWNzZ3akxhdG5XcE0wVjRVQ2YwN3dqMjY3Z0JYaDlxdTZ3SGVTNzlBPT0iLCJtYWMiOiIwZDdlZDc1NjA1MDBhOWM3ZjNlNzU3NTAzZGY3Zjc5NDZjMjMwOGE2MjE5NjUxMzcwZjQ2NDVlMjY4YjAxMmE3In0%3D";
+$headers[] = "Host: geevv.com:443"
+$headers[] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+$headers[] = "Accept-Encoding: gzip, deflate, br"
+$headers[] = "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"
+$headers[] = "Cookie: __cfduid=db24e5b6adcefa7f686db45ce054660fb1541067313; dashboard-popup=1; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ik1zaEVBbUh4aGtnajFVYzNWckJiUEE9PSIsInZhbHVlIjoicmNcL0d4NUpmRWc1VitBVno0d0JFcDBBeEEzcVdDTlZvc0p1N2RyYnp4SVhZZDI5ajFoZnI1TTFXeVcxN2ZHTG9HK1ZNRUViMWxmTzB4OU1Ic0dtM21IMXZ1RUl5Q3R0MDJFQm5vOHp0dFFVPSIsIm1hYyI6ImU5MjU4YmU3ODg5NWE4YWRkYjBmZGMxZTkxMmYwOTAyMTRmNWQ4NDc0YjVmMWYyOTFmMWQyNTc4NDYzMzg3ZjgifQ%3D%3D; laravel_session=d3bfe926711c8b5a6246904268fccae95d3ec7a0; XSRF-TOKEN=eyJpdiI6IlhDcTNZWWJPVWR2K3Z6V0VUVGk4anc9PSIsInZhbHVlIjoiWXlKdnd3R2NWUFgxQ0lmNG9ZWFR5aGN0TEVJQUlcLzNreHhqdTNHMG80dUtQUUJqaXdYNWpoemMwQVQzOTBiYU1XdjRZcTFWQldDRDVtZkh1TTVrZ1lBPT0iLCJtYWMiOiI5MzYzMGQ3NWViNGQ4ZDQ3NTA0NDQ3M2RiMGEzYzUwMmQ5ZTg4ZjhhMTBkYmIwMjkzYzBkYzhjOWU4NTllNzkzIn0%3D"
+$headers[] = "Referer: https://geevv.com/search?q=MujurID&type=web"
+$headers[] = "Upgrade-Insecure-Requests: 1"
+$headers[] = "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 for( $i = 0; $i<5; $i++ ) {
 $result = curl_exec($ch);
