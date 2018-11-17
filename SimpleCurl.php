9d22389bdf872d16d2126f6d20fcaa323368a314
MujurID <40834757+MujurID@users.noreply.github.com>
2018-11-17 20:33:57 +0700

    Add files via upload

--- SimpleCurl.php
+++ SimpleCurl.php
@@ -1,4 +1,5 @@
 <?php
+while(1){
 $handle = curl_init();
  
 $url = "https://shopee.co.id/50000-IDR-Google-Play-Gift-Card-Code-i.78114467.1667404789";
@@ -6,10 +7,11 @@ $url = "https://shopee.co.id/50000-IDR-Google-Play-Gift-Card-Code-i.78114467.166
 // Set the url
 curl_setopt($handle, CURLOPT_URL, $url);
 // Set the result output to be a string.
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
  
 $output = curl_exec($handle);
  
 curl_close($handle);
  
-echo $output;
+//echo $output;
+}
