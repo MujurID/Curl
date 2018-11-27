445b6c9f394cf4b70e817d5db797b0fc83351158
MujurID <40834757+MujurID@users.noreply.github.com>
2018-11-27 15:50:59 +0700

    Update SimpleCurl.php

--- SimpleCurl.php
+++ SimpleCurl.php
@@ -1,17 +1,9 @@
 <?php
-while(1){
-$handle = curl_init();
- 
-$url = "https://shopee.co.id/50000-IDR-Google-Play-Gift-Card-Code-i.78114467.1667404789";
- 
-// Set the url
-curl_setopt($handle, CURLOPT_URL, $url);
-// Set the result output to be a string.
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
- 
-$output = curl_exec($handle);
- 
-curl_close($handle);
- 
-//echo $output;
+while(1){ // unlimited loop
+$handle = curl_init(); // Create a new cURL resource
+$url = "https://shopee.co.id/50000-IDR-Google-Play-Gift-Card-Code-i.78114467.1667404789"; // Set the url
+curl_setopt($handle, CURLOPT_URL, $url); // The URL to fetch. This can also be set when initializing a session with curl_init()
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1); // TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it directly
+$output = curl_exec($handle); // Grab URL and pass it to the browser
+curl_close($handle); // Close cURL resource, and free up system resources
 }
