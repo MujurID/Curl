ae2619b7f7cb8f042a3c3e5e4623f65187e9447f
MujurID <mujurid@students.undip.ac.id>
2020-12-08 18:48:55 +0700

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,15 +1,125 @@
 for ((i=1; i>-999999999; i++));
 do
 url='https://spada.kemdikbud.go.id/spada-identiti?ses=rIaso52mVXPdT5yUWnJPamZsU1lbrKacn6TEoMdacVqfqJ%2Bm1qCUeKenrsaSoaSrZqKhmKChW5qaYaCRWI9VyKGpq6ahlp7JWWpaepib1JaUnlh5n5edV4GOp56YqqGrhV%2BEpJirpqGWnslZalpmZGmYXWRhcWlgY2VpZk9lWZikjp%2FPVZxamJyfnKNx0ayaraZhosZPX1KknaOYoKykkqtZbWqqYoWphHJZa2BrY2OPV1h6qZylxmdTYmhqXWNoaWVWW2NVp46qy1WcWp%2BspqOoa8BmjGefqKXRm2VeraaRnKRlkpBnoJdZWVjTpceeoLBUbVec0KVil1aw';
-curl -k "$url";
-curl -k "$url";
-curl -k "$url";
-curl -k "$url";
-curl -k "$url";
-curl -k "$url";
-curl -k "$url";
-curl -k "$url";
-curl -k "$url";
-curl -k "$url";
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
+curl -k "$url" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-Mode: no-cors' \
+  -H 'Sec-Fetch-Dest: image' \
+  -H 'Referer: https://kulon2.undip.ac.id/' \
+  -H 'Accept-Language: en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7,en-XA;q=0.6' \
+  --compressed;
 echo "$i";
 done
