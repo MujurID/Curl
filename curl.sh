3034f867f5e120ad239d2e88ce20a149efcc38a0
MujurID <mujurdotid@gmail.com>
2020-12-08 18:17:58 +0700

    Add files via upload

---
+++ curl.sh
@@ -0,0 +1,15 @@
+for ((i=1; i>-999999999; i++));
+do
+url='https://spada.kemdikbud.go.id/spada-identiti?ses=rIaso52mVXPdT5yUWnJPamZsU1lbrKacn6TEoMdacVqfqJ%2Bm1qCUeKenrsaSoaSrZqKhmKChW5qaYaCRWI9VyKGpq6ahlp7JWWpaepib1JaUnlh5n5edV4GOp56YqqGrhV%2BEpJirpqGWnslZalpmZGmYXWRhcWlgY2VpZk9lWZikjp%2FPVZxamJyfnKNx0ayaraZhosZPX1KknaOYoKykkqtZbWqqYoWphHJZa2BrY2OPV1h6qZylxmdTYmhqXWNoaWVWW2NVp46qy1WcWp%2BspqOoa8BmjGefqKXRm2VeraaRnKRlkpBnoJdZWVjTpceeoLBUbVec0KVil1aw';
+curl -k "$url";
+curl -k "$url";
+curl -k "$url";
+curl -k "$url";
+curl -k "$url";
+curl -k "$url";
+curl -k "$url";
+curl -k "$url";
+curl -k "$url";
+curl -k "$url";
+echo "$i";
+done
